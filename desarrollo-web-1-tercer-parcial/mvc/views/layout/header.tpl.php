346e6fbac1dbbff9ed71dc1ef155b092f43f913b
PhoenixCho <60702766+PhoenixCho@users.noreply.github.com>
2020-05-06 00:20:11 -0600

    Add files via upload

---
+++ desarrollo-web-1-tercer-parcial/mvc/views/layout/header.tpl.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aprendiendo MVC con PHP</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/bulma.css">
+</head>
+<body>
+    <header>
+        <h1><?php echo $title; ?></h1>
+    </header>
+    <nav>
+        <!-- TODO: navbar -->
+    </nav>
+    <main>
